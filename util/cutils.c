cfeecbc71d015ff79e3f3b7f58b0fdc3c1a2eb74
Carlos L. Torres <carlos.torres@rackspace.com>
2015-07-07 15:40:40 -0500

    cutils: Add qemu_strtoul() wrapper
    Add wrapper for strtoul() function. Include unit tests.
    
    Signed-off-by: Carlos L. Torres <carlos.torres@rackspace.com>

--- util/cutils.c
+++ util/cutils.c
@@ -416,6 +416,33 @@ int qemu_strtol(const char *nptr, const char **endptr, int base,
     }
     return err;
 }
+
+/**
+ * Converts ASCII string to an unsigned long integer.
+ *
+ * If string contains a negative number, value will be converted to
+ * the unsigned representation of the signed value.
+ *
+ * See qemu_strtol() documentation for more info.
+ */
+int qemu_strtoul(const char *nptr, const char **endptr, int base,
+                 unsigned long *result)
+{
+    char *p;
+    int err = 0;
+    if (!nptr) {
+        if (endptr) {
+            *endptr = nptr;
+        }
+        err = -EINVAL;
+    } else {
+        errno = 0;
+        *result = strtoul(nptr, &p, base);
+        err = check_strtox_error(endptr, p, errno);
+    }
+    return err;
+}
+
 /**
  * parse_uint:
  *
